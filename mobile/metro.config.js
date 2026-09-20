2cc826e7d7871b578d8eb14468c467bef6321f86
Kartellxrd <132717817+Kartellxrd@users.noreply.github.com>
2026-09-20 10:58:27 +0000

    fixed

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,1 +1,5 @@
-const{getDefaultConfig,mergeConfig}=require("@react-native/metro-config");module.exports=mergeConfig(getDefaultConfig(__dirname),{});
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
